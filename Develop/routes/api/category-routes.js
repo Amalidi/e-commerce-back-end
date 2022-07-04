8a5656c850edd8478e42025e1ca47327d2c56484
Amalidi <A.idi12@outlook.com>
2022-07-04 21:41:44 +0100

    new category with try catch

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -19,13 +19,45 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", async (req, res) => {
+  try {
+    // find one category by its `id` value
+    const singleCategory = await Category.findByPk(req.params.id, {
+      // be sure to include its associated Products
+      include: [
+        {
+          model: Product,
+          attributes: ["product_name", "price", "stock"],
+        },
+      ],
+    });
+
+    if (!singleCategory) {
+      return res.status(404).json({
+        error: "Category does not exist",
+      });
+    }
+
+    return res.status(200).json(singleCategory);
+  } catch (error) {
+    return res.status(500).json({
+      error: "Failed to get category",
+    });
+  }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+    return res.status(200).json({
+      message: "A new category has been successfully created.",
+      category: newCategory,
+    });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create category. | ${error.message}`);
+    return res.status(500).json({ success: false, error: error.message });
+  }
 });
 
 router.put("/:id", (req, res) => {
